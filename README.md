565a49566b0860b6366c2829b263dfa395db792b
Jeffrey Lowy <jeffreylowy@gmail.com>
2019-03-04 21:55:23 -0800

    First prep for 2019 update

--- README.md
+++ README.md
@@ -1,52 +1,65 @@
-# ALC 2018 Event App
+# ALC 2019 Event App
 
 Table of Contents<br>
+
+- [Get Started](#get-started)<br>
 - [Annual Update Checklist](#annual-update-checklist)<br>
-- [Import Data to Firebase via Firebase Console](#import-data-to-firebase-via-firebase-console)<br>
+- [Firebase Realtime Database](#firebase-realtime-database)<br>
 - [Import Data to Firebase via Command Line](#import-data-to-firebase-via-command-line)<br>
-- [Write data offline from Firebase Docs](#write-data-offline-from-firebase-docshttpsfirebasegooglecomdocsdatabasewebread-and-write)<br>
 - [Generating Icons and Splash Images](#generating-icons-and-splash-images)
 
-### Annual Update Checklist
- - Update the following properties in www/js/app.js:
- 	- *fr_id* - This is the ID number assigned to the TeamRaiser
- 	- *type_id* - Participation type IDs. The ID numbers are found in the TeamRaiser settings under `Participation Types`.
- 	- *group_id.pom_* - Update each POM with their corresponding `group ID` in Luminate Online.
- 	- *group_id.medical* - Update the medical property with it's corresponding `group ID` in Luminate Online.
- - Update next year TeamRaiser registration link (www/templates/menu.html)
- - Clock (*homeCtrl* -www/js/controllers.js)
- - Update the incentives and bike parking Firebase databases.
+## Get Started
 
- ### Updating the app for Ionic & Cordova Builds
+...
 
-Every year there is alway an issue an updating and deploying the app for iOS. Here is what I had to do for the 2018 app.
+## Annual Update Checklist
+
+1. Update the following properties in www/js/app.js:
+
+   - `fr_id` - This is the ID number assigned to the TeamRaiser
+
+   - `type_id` - Participation type IDs. The ID numbers are found in the TeamRaiser settings under `Participation Types`.
+
+   - `group_id.pom` - Update each POM with their corresponding `group ID` in Luminate Online.
+
+   - `group_id.medical` - Update the medical property with it's corresponding `group ID` in Luminate Online.
+
+2. Update next year's TeamRaiser registration link `www/templates/menu.html`
+
+3. The countdowb clock `homeCtrl -www/js/controllers.js`
+
+4. Update the Incentives Firebase database.
+
+5. Update the Bike Parking Firebase database.
+
+## Updating the app for Ionic & Cordova Builds
+
+**_Every year there is always an issue an updating and deploying the app for iOS_**. Here is what I had to do for the 2018 and 2019 apps.
 
 - First, save the currently installed Cordova plugins `cordova plugin save`
+
 - Remove the iOS platform `cordova platform rm ios`
+
+- Remove the Android platform `cordova platform rm android`
+
 - View the installed Cordova plugins `cordova plugin list`
-- The [release of cordova-ios (4.5) requires that the cordova-plugin-console plugin be removed](https://forum.ionicframework.com/t/ionic-not-building-for-ios-linker-command-failed-error-65/91481/7) from your project in order to build since it is now integrated directly `cordova plugin rm cordova-plugin-console`
+
+- The [release of cordova-ios (4.5+) requires that the cordova-plugin-console plugin be removed](https://forum.ionicframework.com/t/ionic-not-building-for-ios-linker-command-failed-error-65/91481/7) from your project in order to build since it is now integrated directly `cordova plugin rm cordova-plugin-console`
+
 - If issues persist, remove and re-install each of the Cordova plugins `cordova plugin rm cordova-plugin-<name-of-plugin>`
+
 - Reinstall the iOS platform `cordova platform add ios`
-- Finally, run `ionic cordova build ios --prod`
 
-For more help, run `cordova --help` or `cordova plugin --help`
+- Reinstall the Android platform `cordova platform add android`
 
-### Import Data to Firebase via Firebase Console
+- Finally, run `ionic cordova build [platform-name] --prod` -where `platform-name` is `ios` or `android`.
 
-### Import Data to Firebase via Command Line
+**_For more info/help, run_** `cordova --help` or `cordova plugin --help`
 
-1) Open the CSV in Sublime Text
-2) Using the `DataConvter` Plugin, choose the option labled `DataConverter: to JSON (first column as key)`
-3) Enter the following command in the terminal:
-	- Replace `<project-name`> with the name of the database.
-	- Replace the `path` with the path you want to update/replace
-	- Replace the `./demo-firebase-import.json` with the path to the JSON file.
-```
-firebase-import --database_url https://<project-name>.firebaseio.com --path /demo --json ./demo-firebase-import.json
-```
-See the [firebase-import](https://github.com/firebase/firebase-import) repo for more information.
+## Firebase Realtime Database
 
-### Write data offline (from [Firebase Docs](https://firebase.google.com/docs/database/web/read-and-write))
+[Firebase Docs](https://firebase.google.com/docs/database/web/read-and-write)
+
 If a client loses its network connection, your app will continue functioning correctly.
 
 Every client connected to a Firebase database maintains its own internal version of any active data. When data is written, it's written to this local version first. The Firebase client then synchronizes that data with the remote database servers and with other clients on a "best-effort" basis.
@@ -55,28 +68,42 @@ As a result, all writes to the database trigger local events immediately, before
 
 Once connectivity is reestablished, your app receives the appropriate set of events so that the client syncs with the current server state, without having to write any custom code.
 
-Note: The Firebase Realtime Database web APIs do not persist data offline outside of the session. In order for writes to be persisted to the server, the web page must not be closed before the data is written to the server.
+**_\*Note: The Firebase Realtime Database web APIs do not persist data offline outside of the session. In order for writes to be persisted to the server, the web page must not be closed before the data is written to the server._**
 
-### Generating Icons and Splash Images
+## Import Data to Firebase via Command Line
+
+1. Open the CSV in Sublime Text
+
+2. Using the `DataConvter` Plugin, choose the option labled `DataConverter: to JSON (first column as key)`
+
+3. Enter the following command in the terminal: - Replace `<project-name`> with the name of the database. - Replace the `path` with the path you want to update/replace - Replace the `./demo-firebase-import.json` with the path to the JSON file.
+
+```
+firebase-import --database_url https://<project-name>.firebaseio.com --path /demo --json ./demo-firebase-import.json
+```
+
+See the [firebase-import](https://github.com/firebase/firebase-import) repo for more information.
+
+## Generating Icons and Splash Images
 
 Usage:<br>
 `$ ionic cordova resources [<platform>] [options]`
 
-*__Note__: By default, this command will not regenerate resources whose source image has not changed. To disable this functionality and always overwrite generated images, use --force.*
+_**Note**: By default, this command will not regenerate resources whose source image has not changed. To disable this functionality and always overwrite generated images, use --force._
 
-Ionic can automatically generate perfectly sized icons and splash screens from source images (.png, .psd, or .ai) for your Cordova platforms. 
+Ionic can automatically generate perfectly sized icons and splash screens from source images (.png, .psd, or .ai) for your Cordova platforms.
 
-* The source image for icons should ideally be at least 1024×1024px and located at resources/icon.png.
-* The source image for splash screens should ideally be at least 2732×2732px and located at resources/splash.png.
+- The source image for icons should ideally be at least 1024×1024px and located at resources/icon.png.
+- The source image for splash screens should ideally be at least 2732×2732px and located at resources/splash.png.
 
- Options:
+Options:
 
     --force, -f .............. Force regeneration of resources
     --icon, -i ............... Generate icon resources
     --splash, -s ............. Generate splash screen resources
 
-  Examples:
+Examples:
 
-`    $ ionic cordova resources`<br>
-`    $ ionic cordova resources ios`<br>
-`    $ ionic cordova resources android`
+`$ ionic cordova resources`<br>
+`$ ionic cordova resources ios`<br>
+`$ ionic cordova resources android`
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,44 +1,55 @@
-angular.module('app.controllers', [])
+// @ts-ignore
+angular
+  .module("app.controllers", [])
 
-  .controller('homeCtrl', ['$scope', '$stateParams', 
-    function ($scope, $stateParams) {
-
-      var deadline = new Date('June 3, 2018 7:00:00');
+  .controller("homeCtrl", [
+    "$scope",
+    "$stateParams",
+    // @ts-ignore
+    function($scope, $stateParams) {
+      var deadline = new Date("June 3, 2019 7:00:00");
 
       function getTimeRemaining(endtime) {
+        var now = new Date();
+        // @ts-ignore
         var t = Date.parse(endtime) - Date.parse(new Date());
         var seconds = Math.floor((t / 1000) % 60);
         var minutes = Math.floor((t / 1000 / 60) % 60);
         var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
         var days = Math.floor(t / (1000 * 60 * 60 * 24));
         return {
-          'total': t,
-          'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
+          total: t,
+          days: days,
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds
         };
       }
 
       function initializeClock(id, endtime) {
         var clock = document.getElementById(id);
-        var daysSpan = clock.querySelector('.days');
-        var hoursSpan = clock.querySelector('.hours');
-        var minutesSpan = clock.querySelector('.minutes');
-        var secondsSpan = clock.querySelector('.seconds');
+        var daysSpan = clock.querySelector(".days");
+        var hoursSpan = clock.querySelector(".hours");
+        var minutesSpan = clock.querySelector(".minutes");
+        var secondsSpan = clock.querySelector(".seconds");
         var today = new Date();
 
         function updateClock() {
           var t = getTimeRemaining(endtime);
+          // @ts-ignore
           daysSpan.innerHTML = t.days;
-          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+          hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+          minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
 
           if (today > deadline) {
+            // @ts-ignore
             daysSpan.innerHTML = 0;
+            // @ts-ignore
             hoursSpan.innerHTML = 0;
+            // @ts-ignore
             minutesSpan.innerHTML = 0;
+            // @ts-ignore
             secondsSpan.innerHTML = 0;
           }
 
@@ -50,58 +61,88 @@ angular.module('app.controllers', [])
         updateClock();
         var timeinterval = setInterval(updateClock, 1000);
       }
-      initializeClock('clockdiv', deadline);
+      initializeClock("clockdiv", deadline);
     }
   ])
 
-  .controller('menuCtrl', function ($scope, $rootScope, $state, $stateParams, $http, $ionicSideMenuDelegate) {
-
-    $scope.logout = function () {
-
+  .controller("menuCtrl", function(
+    $scope,
+    $rootScope,
+    $state,
+    // @ts-ignore
+    $stateParams,
+    $http,
+    $ionicSideMenuDelegate
+  ) {
+    $scope.logout = function() {
       $http({
-        method: 'POST',
-        url: $rootScope.luminate.uri + 'CRConsAPI',
-        data: 'method=logout' + $rootScope.luminate.postdata,
+        method: "POST",
+        url: $rootScope.luminate.uri + "CRConsAPI",
+        data: "method=logout" + $rootScope.luminate.postdata,
         headers: $rootScope.luminate.header
-      }).then(function () {
+      }).then(
+        function() {
+          $rootScope.luminate.loggedIn = false;
+          $rootScope.luminate.cons_id = "";
+          $rootScope.luminate.token = "";
+          $rootScope.luminate.tr_info = {};
+          $rootScope.luminate.bikeParking = false;
+          $rootScope.luminate.groups = {
+            pom: "NO RSVP",
+            med_form: false,
+            incentive_pickup: false
+          };
 
-        $rootScope.luminate.loggedIn = false;
-        $rootScope.luminate.cons_id = '';
-        $rootScope.luminate.token = '';
-        $rootScope.luminate.tr_info = {};
-        $rootScope.luminate.bikeParking = false;
-        $rootScope.luminate.groups = {
-          pom: 'NO RSVP',
-          med_form: false,
-          incentive_pickup: false
-        };
-
-        $ionicSideMenuDelegate.toggleLeft();
-        $state.go('menu.home');
-      }, function () {
-        console.log('Log out failed.');
-      });
-    }
+          $ionicSideMenuDelegate.toggleLeft();
+          $state.go("menu.home");
+        },
+        function() {
+          console.log("Log out failed.");
+        }
+      );
+    };
   })
 
-  .controller('loginCtrl', function ($scope, $ionicPopup, $state, $stateParams, $rootScope, $http, $log, loginService, constituentService, teamRaiserService, tentMateService, participantProgress, constituentGroupsService, interactionService) {
-
+  .controller("loginCtrl", function(
+    $scope,
+    $ionicPopup,
+    $state,
+    // @ts-ignore
+    $stateParams,
+    $rootScope,
+    $http,
+    // @ts-ignore
+    $log,
+    // @ts-ignore
+    loginService,
+    constituentService,
+    teamRaiserService,
+    tentMateService,
+    participantProgress,
+    constituentGroupsService,
+    interactionService
+  ) {
     // Form data for the login modal
     $scope.loginData = {};
-    $scope.loginData.username = window.localStorage.username || '';
-    $scope.loginData.password = window.localStorage.password || '';
+    $scope.loginData.username = window.localStorage.username || "";
+    $scope.loginData.password = window.localStorage.password || "";
     $scope.savePassword = true;
 
-    $scope.login = function () {
-
+    $scope.login = function() {
       //LOGIN REQUEST
       $http({
-          method: 'POST',
-          url: $rootScope.luminate.uri + 'CRConsAPI',
-          data: 'method=login' + $rootScope.luminate.postdata + '&user_name=' + $scope.loginData.username + '&password=' + $scope.loginData.password,
-          headers: $rootScope.luminate.header
-        })
-        .success(function (loginResponseData) {
+        method: "POST",
+        url: $rootScope.luminate.uri + "CRConsAPI",
+        data:
+          "method=login" +
+          $rootScope.luminate.postdata +
+          "&user_name=" +
+          $scope.loginData.username +
+          "&password=" +
+          $scope.loginData.password,
+        headers: $rootScope.luminate.header
+      })
+        .success(function(loginResponseData) {
           //Store the login Success response in the response variable
           var response = loginResponseData.loginResponse;
 
@@ -117,16 +158,15 @@ angular.module('app.controllers', [])
 
           $rootScope.luminate.loggedIn = true;
         })
-        .error(function (errorResponse) {
-          console.log('Log in error:', errorResponse);
+        .error(function(errorResponse) {
+          console.log("Log in error:", errorResponse);
           $rootScope.luminate.loggedIn = false;
           $ionicPopup.alert({
-            title: 'Alert!',
-            template: 'Username or password is incorrect.'
+            title: "Alert!",
+            template: "Username or password is incorrect."
           });
         })
-        .then(function () {
-
+        .then(function() {
           //Get Constituent Profile
           constituentService.getConsRecord();
           //Get TeamRaiser Registration
@@ -137,31 +177,40 @@ angular.module('app.controllers', [])
           participantProgress.getProgress();
           //Constituent Groups
           constituentGroupsService.getGroups();
-
-        }).then(function () {
-          setTimeout(function () {
-            $state.go('menu.home');
+        })
+        .then(function() {
+          setTimeout(function() {
+            $state.go("menu.home");
           }, 500);
 
-          interactionService.logInteraction('My ALC Spin login', '*** Test ***\nLogged in with My ALC Spin app');
+          interactionService.logInteraction(
+            "My ALC Spin login",
+            "*** Test ***\nLogged in with My ALC Spin app"
+          );
         });
-
     };
-
   })
 
-  .controller('checkInCtrl', function ($scope, $stateParams) {})
+  // @ts-ignore
+  .controller("checkInCtrl", function($scope, $stateParams) {})
 
-  .controller('orientationDayCtrl', function ($scope, $stateParams, $rootScope) {
-
-    JsBarcode('#alc-num-barcode', $rootScope.luminate.tr_info.raceNumber, {
+  // @ts-ignore
+  .controller("orientationDayCtrl", function($scope, $stateParams, $rootScope) {
+    // @ts-ignore
+    JsBarcode("#alc-num-barcode", $rootScope.luminate.tr_info.raceNumber, {
       width: 3
     });
-
   })
 
-  .controller('incentivesCtrl', function ($scope, $rootScope, $stateParams, $http, incentivesService) {
-
+  .controller("incentivesCtrl", function(
+    $scope,
+    $rootScope,
+    // @ts-ignore
+    $stateParams,
+    // @ts-ignore
+    $http,
+    incentivesService
+  ) {
     /**
      * INCENTIVE KEYS:
      * I_15K = All types $15,000
@@ -174,7 +223,7 @@ angular.module('app.controllers', [])
      * I_C1000 = Cyclist $1,000
      * I_C500 = Cyclist 500
      * I_R500 = Roadie 500
-     * I_R100 = Roadie 100 
+     * I_R100 = Roadie 100
      */
 
     $scope.partType = $rootScope.luminate.tr_info.typeName;
@@ -182,52 +231,57 @@ angular.module('app.controllers', [])
     $scope.incentiveDisplay = {};
     $scope.top545Display = {};
     $scope.top50Display = {};
-    
+
     $scope.incentives = incentivesService.getIncentives();
     $scope.top545 = incentivesService.getTop545();
     $scope.top50 = incentivesService.getTop50();
 
-    $scope.incentives.on('value', function (snapshot) {
-      $scope.$apply(function () {
+    $scope.incentives.on("value", function(snapshot) {
+      $scope.$apply(function() {
         $scope.incentiveDisplay = snapshot.val();
       });
     });
 
-    $scope.top545.on('value', function (snapshot) {
-      $scope.$apply(function () {
+    $scope.top545.on("value", function(snapshot) {
+      $scope.$apply(function() {
         $scope.top545Display = snapshot.val();
       });
     });
 
-    $scope.top50.on('value', function (snapshot) {
-      $scope.$apply(function () {
+    $scope.top50.on("value", function(snapshot) {
+      $scope.$apply(function() {
         $scope.top50Display = snapshot.val();
       });
     });
 
-    $scope.incentiveRefresh = function () {
+    $scope.incentiveRefresh = function() {
       $rootScope.refresher();
-      $scope.$broadcast('scroll.refreshComplete');
-    }
-
+      $scope.$broadcast("scroll.refreshComplete");
+    };
   })
 
-  .controller('bikeLocationCtrl', function ($scope, $rootScope, $stateParams, $http, bikeParkingService) {
-
+  .controller("bikeLocationCtrl", function(
+    $scope,
+    $rootScope,
+    // @ts-ignore
+    $stateParams,
+    // @ts-ignore
+    $http,
+    bikeParkingService
+  ) {
     $scope.getBikeInfo = bikeParkingService.getBikeLocation();
     $scope.bike_data = null;
-    
-    $scope.getBikeInfo.on('value', function (snapshot) {
-      $scope.$apply(function () {
+
+    $scope.getBikeInfo.on("value", function(snapshot) {
+      $scope.$apply(function() {
         $scope.bike_data = snapshot.val();
       });
       console.log("Bike data", $scope.bike_data);
     });
 
-    $scope.bikeInfoRefresh = function () {
+    $scope.bikeInfoRefresh = function() {
       $rootScope.refresher();
       $scope.getBikeInfo = bikeParkingService.getBikeLocation();
-      $scope.$broadcast('scroll.refreshComplete');
+      $scope.$broadcast("scroll.refreshComplete");
     };
-
   });
